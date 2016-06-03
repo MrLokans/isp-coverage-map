95aab64cbf41dfb391d9cdc4323cd71ec32a6a2a
MrLokans <trikster1911@gmail.com>
2016-06-03 19:40:21 +0300

    MTS parser is now moved to separate class.

--- mts_parser.py
+++ mts_parser.py
@@ -1,35 +1,22 @@
-# TODO: parse descriptions, show only active
-
 import re
-import csv
 
 import requests
 
-MTS_MAP_URL = "http://www.mts.by/home/connect/"
-
-YA_MAPS_POINT_REGEX = r"var placemark(?P<id>[\d]+) = new YMaps.Placemark\(new YMaps\.GeoPoint\((?P<long>[\d]+\.[\d]+),(?P<lat>[\d]+\.[\d]+)"
+from point import Point
 
 
-def unescape_text(text):
-    unescaped = text.replace(r"\x3c", "<").replace(r"\x3e", ">")
-    unescaped = unescaped.replace(r"\x3d", "=")
-    unescaped = unescaped.replace(r"\x26quot;", "&")
-    return unescaped
+class MTS_Parser(object):
+    PARSER_NAME = "MTS"
+    MTS_MAP_URL = "http://www.mts.by/home/connect/"
+    YA_MAPS_POINT_REGEX = r"var placemark(?P<id>[\d]+) = new YMaps.Placemark\(new YMaps\.GeoPoint\((?P<long>[\d]+\.[\d]+),(?P<lat>[\d]+\.[\d]+)"
 
-
-def main():
-    r = requests.get(MTS_MAP_URL)
-    matches = [x for x in re.findall(YA_MAPS_POINT_REGEX, r.text)]
-    with open('mts.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(["longitude", "latitude"])
-        for _, longitude, latitude in matches:
-            writer.writerow([longitude, latitude])
-
-
-def get_map_points(map_json):
-    return []
+    def get_points(self):
+        """Parse MTS site and extract points from map"""
+        r = requests.get(self.MTS_MAP_URL)
+        matches = (x for x in re.findall(self.YA_MAPS_POINT_REGEX, r.text))
+        return [Point(long, lat, "") for _, long, lat in matches]
 
 
 if __name__ == '__main__':
-    main()
+    parser = MTS_Parser()
+    dots = parser.get_points()
