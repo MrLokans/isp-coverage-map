e5746c9e0fa358ac14f573cc0927f740606a127c
MrLokans <trikster1911@gmail.com>
2016-06-13 07:36:47 +0300

    Extra parsers now made available for import.

--- by_isp_coverage/__init__.py
+++ by_isp_coverage/__init__.py
@@ -1,9 +1,12 @@
+from .atlant_parser import AtlantParser
 from .byfly_parser import ByflyParser
 from .csv_exporter import CSV_Exporter
 from .mts_parser import MTS_Parser
+from .unet_parser import UNETParser
 
 
-__version__ = "0.2"
+__version__ = "0.2.1"
 
 
-__all__ = ["ByflyParser", "CSV_Exporter", "MTS_Parser"]
+__all__ = ["ByflyParser", "CSV_Exporter", "MTS_Parser",
+           "AtlantParser", "UNETParser"]
