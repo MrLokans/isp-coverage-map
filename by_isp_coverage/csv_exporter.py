dbe6e83751a5638956d09a019d2da0ea2ec5dff3
MrLokans <trikster1911@gmail.com>
2016-06-27 19:58:31 +0300

    CSV exporter refactored.

--- by_isp_coverage/csv_exporter.py
+++ by_isp_coverage/csv_exporter.py
@@ -1,13 +1,24 @@
 import csv
 
+from .connection import Connection
+from .point import Point
+
 
 class CSV_Exporter(object):
-    @staticmethod
-    def export_points(filename, points):
+
+    @classmethod
+    def export_namedtuple_values(cls, filename, data, tpl_cls):
+        assert hasattr(tpl_cls, "_fields")
         with open(filename, 'w') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(["longitude", "latitude", "description"])
-            for point in points:
-                writer.writerow([point.longitude,
-                                 point.latitude,
-                                 point.description])
+            writer.writerow(tpl_cls._fields)
+            for elem in data:
+                writer.writerow([getattr(elem, f) for f in tpl_cls._fields])
+
+    @classmethod
+    def export_points(cls, filename, points):
+        cls.export_namedtuple_values(filename, points, Point)
+
+    @classmethod
+    def export_connections(cls, filename, connections):
+        cls.export_namedtuple_values(filename, connections, Connection)
