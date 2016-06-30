09843bbcaca93c1b365da571edd099ac0507f9f8
MrLokans <trikster1911@gmail.com>
2016-06-30 09:33:15 +0300

    Python 2 support dropped.

--- by_isp_coverage/parsers/mts_parser.py
+++ by_isp_coverage/parsers/mts_parser.py
@@ -11,15 +11,23 @@ class MTS_Parser(BaseParser):
     PARSER_URL = "http://www.mts.by"
     MTS_MAP_URL = "http://www.mts.by/home/connect/"
     YA_MAPS_POINT_REGEX = r"var placemark(?P<id>[\d]+) = new YMaps.Placemark\(new YMaps\.GeoPoint\((?P<long>[\d]+\.[\d]+),(?P<lat>[\d]+\.[\d]+)"
+    YA_MAPS_POINT_NAME_REGEX = r'placemark(?P<id>[\d]+)\.name = "(?P<name>[^\']*)";'
+    YA_MAPS_POINT_DESCRIPTION_REGEX = r'placemark(?P<id>[\d]+)\.description = "(?P<description>[^\']*)";'
 
     def __init__(self, *args, **kwargs):
         pass
 
+    def _extract_point_name(self, point_id):
+        pass
+
+    def _coordinates_from_map(self, page_content):
+        return ((c[1], c[2]) for c in re.finditer(self.YA_MAPS_POINT_REGEX,
+                                                  page_content))
+
     def get_points(self):
         """Parse MTS site and extract points from map"""
         r = requests.get(self.MTS_MAP_URL)
-        matches = (x for x in re.findall(self.YA_MAPS_POINT_REGEX, r.text))
-        return (Point(long, lat, "") for _, long, lat in matches)
+        return (Point(*c, "") for c in self._get_coordinates_from_map(r.text))
 
 
 if __name__ == '__main__':
